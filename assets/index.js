6bb7c1672435862fb9b9ba64d27160f078d92d99
Gabriela Sosa <sosagabriela777@gmail.com>
2024-02-10 01:08:24 -0300

    punto 3 listo, sin estilos

--- assets/index.js
+++ assets/index.js
@@ -3,17 +3,19 @@ var capa;
 var Point;
 var SimpleMarkerSymbol;
 var Graphic;
+var PopupTemplate;
+
 require([
     "esri/map",
     "esri/layers/GraphicsLayer",
     "esri/graphic",
     "esri/symbols/SimpleMarkerSymbol",
     "esri/geometry/Point",
-    "esri/dijit/Popup",
-    'dojo/dom',
+    "esri/dijit/PopupTemplate",
+    "dojo/dom",
     "dojo/domReady!"
 ], function (
-    Map, GraphicsLayer, EsriGraphic, EsriSimpleMarkerSymbol, EsriPoint
+    Map, GraphicsLayer, EsriGraphic, EsriSimpleMarkerSymbol, EsriPoint, EsriPopupTemplate
 ) {
     mapa = new Map("map", {
         center: [-58.3724715, -34.595986],
@@ -23,10 +25,11 @@ require([
 
     capa = new GraphicsLayer();
     mapa.addLayer(capa);
-
     Point = EsriPoint;
     SimpleMarkerSymbol = EsriSimpleMarkerSymbol;
-    Graphic = EsriGraphic; 
+    Graphic = EsriGraphic;
+    PopupTemplate = EsriPopupTemplate;
+
     document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("poiButton").onclick = agregarPunto;
     });
@@ -42,12 +45,11 @@ function agregarPunto() {
 
     var punto = new Point(parseFloat(coord[1]), parseFloat(coord[0]));
     var simbolo = new SimpleMarkerSymbol().setStyle(SimpleMarkerSymbol.STYLE_CIRCLE).setColor([255, 0, 0, 1]);
-    var graficoPunto = new Graphic( punto, simbolo);
+    var content = "<b>Nombre:</b> " + name + "<br><b>Dirección:</b> " + direction + "<br><b>Teléfono:</b> " + telephone + "<br><b>Categoría:</b> " + category + "<br><b>Coordenadas:</b> " + punto.x + ", " + punto.y;
+    var graficoPunto = new Graphic(punto, simbolo).setInfoTemplate(new esri.InfoTemplate("Punto de Interés", content));
 
     capa.add(graficoPunto);
-
     mapa.centerAt(punto);
-    
 
     console.log("Nombre:", name);
     console.log("Dirección:", direction);
